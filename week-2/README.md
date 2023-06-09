f9d81d41fc391c3cff4284108e3505f690ad7844
keremparlak0 <56314081+keremparlak0@users.noreply.github.com>
2023-06-09 15:46:20 +0300

    update and add readme

---
+++ week-2/README.md
@@ -0,0 +1,15 @@
+# Hafta-2 Ödevi
+### Araç Kiralama Uygulaması
+### Özellikler
+- **Roller**: Bireysel ve Kurumsal olmak üzere iki kullanıcı türü mevcut.
+- **Araç Rolleri**: SUV, Sedan ve Hatchback olmak üzere üç farklı kasa tipine sahip araç türü mevcut. Araçların günlük kiralama ücretleri, çeşitli ortak ve kendine ait özellikleri bulunmakta.
+
+  - Sedan araçlar yaşlarına orantılı olarak fiyat düşüşü yaşarken, Hatchback araçlar ise bagaj kapasitesine bağlı olarak fiyat artışı yaşıyor.
+### Senaryo
+- **Bireysel Kullanıcı**
+    - Bireysel kullanıcı kendisine ait olan; isim, e-posta adresi, şifre ve TC kimlik numarasıı ile giriş yapabiliyor.
+    - Sadece Hatchack tipindeki araçların günlük veya aylık kiralama bedellerini görebiliyor.
+
+- **Kurumsal Kullanıcı**
+    - Kurumsal Kullanıcı şirkete ait olan; isim, e-posta adresi, şifre ve vergi numarası ile giriş yapabiliyor.
+    - Tüm türlerdeki araç tiplerinin fiyatına erişebiliyor.
--- week-2/src/services/RentService.java
+++ week-2/src/services/RentService.java
@@ -6,7 +6,7 @@ import customers.*;
 public class RentService {
     // Overloading kullanarak Bireysel hesapların sadece hatchback araç fiyatı sorabileceği bir yapı kurdum
     public RentService(Company company, Car car, String rentChoice) {
-        if (rentChoice == "gunluk") {
+        if (rentChoice.equals("gunluk")) {
             System.out.println(car.color + " renkli " + car.getClass().getName() +" aracın günlük kiralama ücreti = " + car.rentDaily() + "₺");
         }else {
             System.out.println(car.color + " renkli " + car.getClass().getName() +" aracın aylık kiralama ücreti = " + car.rentMonthly() + "₺");;
@@ -14,7 +14,7 @@ public class RentService {
     }
 
     public RentService(Person person, Hatchback hatchback, String rentChoice) {
-        if (rentChoice == "gunluk") {
+        if (rentChoice.equals("gunluk")) {
             System.out.println(hatchback.color + " renkli " + hatchback.getClass().getName() +" aracın günlük kiralama ücreti = " + hatchback.rentDaily() + "₺");;
         }else {
             System.out.println(hatchback.color + " renkli " + hatchback.getClass().getName() +" aracın aylık kiralama ücreti = " + hatchback.rentMonthly() + "₺");;
